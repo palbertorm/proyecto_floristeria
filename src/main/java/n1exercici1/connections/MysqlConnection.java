9932554d551032a1a9d13bde9b0688ebf2a91b79
pauloalberto <palbertorm35@gmail.com>
2024-02-18 10:47:43 +0100

    WIP SINGLETON

--- src/main/java/n1exercici1/connections/MysqlConnection.java
+++ src/main/java/n1exercici1/connections/MysqlConnection.java
@@ -3,19 +3,40 @@ package n1exercici1.connections;
 import java.sql.*;
 
 public class MysqlConnection {
-    Connection conn = null;
-    Statement stmt = null;
-    ResultSet res = null;
- public void connect() {
+    Connection conn;
+    PreparedStatement stmt;
+    ResultSet res;
+    String sql;
+
+
+    public void connect() {
      try(Connection conn = DriverManager.getConnection("jdbc:mysql//localhost:3306/nombre_bdd", "root", "")){
-      stmt = conn.createStatement();
-      res = stmt.executeQuery("SHOW DATABASE");
-      while (res.next()){
-          System.out.println(res.getString("name"));
-      }
+        String sql = ("INSERT INTO  VALUES ()");
+         stmt = conn.prepareStatement(sql);
      }catch (SQLException e){
          e.printStackTrace();
      }
  }
-
+// public void create(){
+//     try(Connection conn = DriverManager.getConnection("jdbc:mysql//localhost:3306/nombre_bdd", "root", "")){
+//         stmt = conn.prepareStatement(this.sql);
+//     }catch (SQLException e){
+//         e.printStackTrace();
+//     }
+// }
+// public void update(){
+//     try(Connection conn = DriverManager.getConnection("jdbc:mysql//localhost:3306/nombre_bdd", "root", "")){
+//         stmt = conn.prepareStatement("");
+//     }catch (SQLException e){
+//         e.printStackTrace();
+//     }
+// }
+//
+//public void delete(){
+//    try(Connection conn = DriverManager.getConnection("jdbc:mysql//localhost:3306/nombre_bdd", "root", "")){
+//        stmt = conn.prepareStatement("");
+//    }catch (SQLException e){
+//        e.printStackTrace();
+//    }
+//}
 }
--- src/main/java/n1exercici1/services/mysqlDAO/MySQLFlowerDAO.java
+++ src/main/java/n1exercici1/services/mysqlDAO/MySQLFlowerDAO.java
@@ -2,17 +2,22 @@ package n1exercici1.services.mysqlDAO;
 
 import n1exercici1.connections.MysqlConnection;
 import n1exercici1.products.Flower;
+import n1exercici1.products.Product;
 import n1exercici1.services.productsDAO.FlowerDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MySQLFlowerDAO implements FlowerDAO {
     final String INSERT = "INSERT INTO Tabla (col1, clo2) VALUES(?,?)";
     final String UPDATE = "UPDATE Tabla SET nombre = ? WHERE id = ?";
     final String DELETE = "DELETE FROM Tabla WHERE ID = ?";
+    final String GETALL = "SELECT* FROM flower";
+    final String GETONE = " SELECT flower.id FROM flower";
 
     private Connection conn;
 
@@ -30,6 +35,13 @@ public class MySQLFlowerDAO implements FlowerDAO {
     if (stmt.executeUpdate() == 0){
         System.out.println(" was not inserted");
     }
+    try(ResultSet res = stmt.getGeneratedKeys()){
+        if (res.next()) {
+            item.setIdProduct(res.getInt(1));
+        }else {
+            System.out.println("there is no id column");
+        }
+    }
     } catch (SQLException e) {
         throw new RuntimeException(e);
     }
@@ -38,6 +50,10 @@ public class MySQLFlowerDAO implements FlowerDAO {
     @Override
     public void delete(Flower item) {
     try(PreparedStatement stmt = conn.prepareStatement(DELETE)) {
+        stmt.setInt(1, item.getIdProduct());
+        if (stmt.executeUpdate() == 0){
+            System.out.println(" was not deleted");
+        }
 
     } catch (SQLException e) {
         throw new RuntimeException(e);
@@ -46,16 +62,51 @@ public class MySQLFlowerDAO implements FlowerDAO {
 
     @Override
     public void update(Flower item) {
+        try (PreparedStatement stmt = conn.prepareStatement(UPDATE)) {
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private Flower convertir(ResultSet res) throws SQLException {
+        String name = res.getString("name");
+        String color = res.getString("color");
+        double price = res.getDouble("price");
+        Product flower = new Flower(name, price, color);
+        // en caso de id hacer un setId(res.getInt("id"));
+        return (Flower) flower;
     }
 
-    @Override
+        @Override
     public List<Flower> getAll() {
-        return null;
+           List <Flower> flower = new ArrayList<>();
+            try(PreparedStatement stmt = conn.prepareStatement(GETALL)) {
+                try (ResultSet res =  stmt.executeQuery()) {
+                    while (res.next()) {
+                        flower.add(convertir(res));
+
+                    }
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return flower;
     }
 
     @Override
     public Flower getOne(Integer id) {
-        return null;
+        Flower flower = null;
+        try(PreparedStatement stmt = conn.prepareStatement(GETONE)) {
+            stmt.setInt(1, id);
+            try (ResultSet res =  stmt.executeQuery()) {
+                if (res.next()) {
+                    flower = convertir(res);
+
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return flower;
     }
 }
